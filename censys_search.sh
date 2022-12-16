83e267672e5f8c8ee211c23c7f091d7a7e897b18
invoke-eric <106356284+invoke-eric@users.noreply.github.com>
2022-12-16 14:54:02 -0500

    Update censys_search.sh

--- censys_search.sh
+++ censys_search.sh
@@ -1,1 +1,1 @@
-censys search 'services.jarm.fingerprint:07d19d20d21d20d07c07d19d07d20dc18a93d91a46d601d080d49d6ed845c3' --index-type hosts | jq -r '.[] | {ip: .ip} | join("")'
+censys search 'services.jarm.fingerprint:07d19d20d21d20d07c07d19d07d20dc18a93d91a46d601d080d49d6ed845c3' --index-type hosts --pages=-1 | jq -r '.[] | {ip: .ip} | join("")'
