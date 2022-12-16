667f6b7ee4e889f9f3989d7f90472feaed9506b2
invoke-eric <106356284+invoke-eric@users.noreply.github.com>
2022-12-16 14:55:08 -0500

    Create censys_search_TLS_common_names.sh

---
+++ censys_search_TLS_common_names.sh
@@ -0,0 +1,1 @@
+censys search 'services.jarm.fingerprint: 07d19d20d21d20d07c07d19d07d20dc18a93d91a46d601d080d49d6ed845c3' --index-type hosts --pages=-1 | jq -r '.[]|.ip' | while read i ; do censys view "$i" | jq -r '.services[].tls.certificates.leaf_data.subject | objects | .common_name[]'; done
